043f03da7bba0126f39b274e40a27d5a536a7107
jansk32 <jankwo32@gmail.com>
2023-08-04 23:44:44 +1000

    footer

--- resume/src/Layouts/Footer.js
+++ resume/src/Layouts/Footer.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default function Footer() {
     return (
-      <div>
-          <p>Footer</p>
+      <div style={{
+        'backgroundColor' : 'black',
+        'color' : 'white',
+        'margin-top': '5vh',
+        'padding-top': '3vh',
+        'height': '15vh'}}>
+          <p>© 2023 Jansen, All Rights Reserved</p>
       </div>
     )
   }
