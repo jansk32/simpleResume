0f63c646799fc13fe130fdcccf5b4bb561896204
jansk32 <jankwo32@gmail.com>
2023-10-07 11:42:58 +1100

    porfolio uploaded and css done

--- resume/src/Pages/Photography.js
+++ resume/src/Pages/Photography.js
@@ -5,13 +5,34 @@ import { Carousel } from 'react-responsive-carousel';
 import './style/photography.css';
 import SliderItem from "./SliderItem.js";
 
+// photos
+import grandma from "../assets/photos/Grandma Icecream.jpg";
+import photo1 from "../assets/photos/20160524_165215.jpg";
+import photo2 from "../assets/photos/IMG_0752.jpg";
+import photo3 from "../assets/photos/IMG_7202.jpg";
+import photo4 from "../assets/photos/IMG_9950.jpg";
+
+
 function Photography(){
 
     const slideImages = [
         {
-          imgUrl: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+          imgUrl: grandma,
+        },
+        {
+            imgUrl: photo1,
+        },
+        {
+            imgUrl: photo2,
+        },
+        {
+            imgUrl: photo3,
+        },
+        {
+            imgUrl: photo4,
         },
       ];
+
 
 
 
@@ -19,17 +40,17 @@ function Photography(){
         <Layout>
             <div className="bg-black text-white">
                 <div className="photography-banner bg-contain bg-no-repeat bg-center center py-16">
-                    <p className="text-2xl mt-32 pt-32">Your memories are priceless |</p>
+                    <p className="text-2xl mt-32 px-32">Your memories are priceless |</p>
                     <p className="text-9xl">PHOTOGRAPHY.</p>
                 </div>
-                <div className="code-section center">
+                <div className="code-section center pb-32">
                     <div className="code-github">
                         <p className="text-xl">Other photos are available on my <a className="underline underline-offset-3" href="https://www.instagram.com/jansenkwong">Instagram</a></p>
                     </div>
                     <div className="code-slides">
                         <Carousel showArrows={true} centerMode={false} infiniteLoop={true}>
                                   {slideImages.map((item, index) => (
-                              <div className='slider my-slider' key={index}>
+                              <div className='slider my-slider justify-center' key={index}>
                                 <SliderItem url={item.url} imgUrl={item.imgUrl} caption={item.caption} description={item.description} />
                               </div>
                                   ))}
--- resume/src/Pages/Projects.js
+++ resume/src/Pages/Projects.js
@@ -5,35 +5,42 @@ import { Carousel } from 'react-responsive-carousel';
 import './style/portfolio.css';
 import SliderItem from "./SliderItem.js";
 
+// import portfolio
+import crypto from "../assets/portfolio/crypto.png";
+import metro from "../assets/portfolio/metropolitan.png";
+import mementos from "../assets/portfolio/mementos.jpeg";
+import gps from "../assets/portfolio/gps.jpeg";
+import tipover from "../assets/portfolio/tipover.png";
+
 function Portfolio(){
 
     const slideImages = [
         {
-          imgUrl: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+          imgUrl: crypto,
           caption: 'Crypto Task',
-          description: 'This is a project to create a listing of all available crypto currencies, to be able to sort each column and is paginated for ease of viewing ',
+          description: 'This is a project to create a listing of all available crypto currencies, be able to search for certain coins, to sort based on individual columns and is paginated for ease of viewing ',
           url: "https://github.com/jansk32/CryptoTaskPage"
         },
         {
-            imgUrl: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+            imgUrl: gps,
             caption: 'Get People Socializing',
             description: "This is a project to create a social web application to connect people new to the area with locals with similar interests using an interactive map that points out upcoming events and places of interest based on user's preference. Worked in a group of 4, and was submitted as a group assignment.",
             url: "https://github.com/StaticDDQ/Go-Blox-GPS"
         },
         {
-            imgUrl: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+            imgUrl: mementos,
             caption: 'Mementos',
             description: 'This is a project to create an app that stores data of family antiques / artefacts, family tree, and a timeline of those artefacts. Worked in a group of 4, and the app runs on both android and iOS',
             url: "https://github.com/jansk32/mementos"
         },
         {
-            imgUrl: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+            imgUrl: metro,
             caption: 'Metropolitan Museum Web Hightlight',
             description: 'Using the Metropolitan Museum API, this project creates a highlight page of some of the artworks available, and provides information on each of them. This was coded in React',
             url: "https://github.com/jansk32/MetropolitanView"
         },
         {
-            imgUrl: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+            imgUrl: tipover,
             caption: 'Tipover Game Solver',
             description: 'Based on the puzzle game developed by ThinkFun games, this is program that solves the puzzles using A* search',
             url: "https://github.com/jansk32/TipOverGame"
@@ -56,7 +63,7 @@ function Portfolio(){
                     <div className="code-slides">
                         <Carousel showArrows={true} centerMode={false} infiniteLoop={true}>
                                   {slideImages.map((item, index) => (
-                              <div className='slider my-slider' key={index}>
+                              <div className='slider my-slider justify-center' key={index}>
                                 <SliderItem url={item.url} imgUrl={item.imgUrl} caption={item.caption} description={item.description} />
                               </div>
                                   ))}
