19d05241ae92b9eaf05e14dc00614f0a4a1fdfc7
jansk32 <jankwo32@gmail.com>
2023-10-23 10:48:13 +1100

    cors pt 3

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
+app.use(express.static(path.join(__dirname, '../client/build')))
 
 async function storeDetails(body, messageId) {
         // store details in db
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app.js')
 
-const port = process.env.port || 5000;
+const port = process.env.port || 3500;
 
 app.listen(port);
